6727bdb3ca05a09f9cd3c77c897d2b333500a8f6
3b-fly <member@3b-fly.eu>
2018-07-23 07:25:47 +0200

    Added "bbbfly_DateTime" class
    Supports big integer DateTime representation to suppress integer range
    limitation.
    
    Range = +- (10^11) - 1 years
    Precision = 1 minute

---
+++ build/common/bbbfly_DateTime.php
@@ -0,0 +1,66 @@
+<?php
+/*!
+ * @author Jan Nejedly support@3b-fly.eu
+ * @copyright Jan Nejedly
+ * @version 1.0.0
+ * @license see license in 'LICENSE' file
+*/
+?>
+<?php
+class bbbfly_DateTime extends DateTime
+{
+  const BI_CALENDAR_GREGORIAN = 1;
+
+  public function setBigInt($biString){
+    if(!is_string($biString)){
+      throw new Exception('Wrong parameter type.');
+    }
+
+    $pattern = array(
+      'bc' => '(-?)',
+      'cal' => '([1-8]{1})',
+      'year' => '([0-9]{10})',
+      'month' => '([0-9]{2})',
+      'day' => '([0-9]{2})',
+      'hour' => '([0-9]{2})',
+      'minute' => '([0-9]{2})'
+    );
+    $pattern = '~^'.implode('',$pattern).'$~';
+
+    $parts = array();
+    if(!preg_match($pattern,$biString,$parts)){
+      throw new Exception('Wrong parameter format.');
+    }
+
+    switch($parts[2]){
+      case self::BI_CALENDAR_GREGORIAN: continue;
+      default: throw new Exception('Unsupported calendar type.');
+    }
+
+    $errorLevel = error_reporting(0);
+    $this->setTimezone('UTC');
+    error_reporting($errorLevel);
+
+    $year = (double)$parts[3];
+    $month = (int)$parts[4];
+    $day = (int)$parts[5];
+    $hour = (int)$parts[6];
+    $minute = (int)$parts[7];
+
+    if($parts[1]){$year *= -1;}
+
+    $this->setDate($year,$month,$day);
+    $this->setTime($hour,$minute,0);
+  }
+
+  public function toBigInt(){
+    $calendar = self::BI_CALENDAR_GREGORIAN;
+
+    $year = $this->format('Y');
+    $before = (substr($year,0,1) === '-') ? '-' : '';
+    if($before){$year = substr($year,1);}
+    $year = sprintf("%010d",$year);
+
+    return $before.$calendar.$year.$this->format('mdHi');
+  }
+}
---
+++ src/common/bbbfly_DateTime.php
@@ -0,0 +1,56 @@
+<?php
+class bbbfly_DateTime extends DateTime
+{
+  const BI_CALENDAR_GREGORIAN = 1;
+
+  public function setBigInt($biString){
+    if(!is_string($biString)){return false;}
+
+    $pattern = array(
+      'bc' => '(-?)',
+      'cal' => '([1-8]{1})',
+      'year' => '([0-9]{10})',
+      'month' => '([0-9]{2})',
+      'day' => '([0-9]{2})',
+      'hour' => '([0-9]{2})',
+      'minute' => '([0-9]{2})'
+    );
+    $pattern = '~^'.implode('',$pattern).'$~';
+
+    $parts = array();
+    if(!preg_match($pattern,$biString,$parts)){return false;}
+
+    switch($parts[2]){
+      case self::BI_CALENDAR_GREGORIAN: continue;
+      default: return false;
+    }
+
+    $errorLevel = error_reporting(0);
+    $this->setTimezone('UTC');
+    error_reporting($errorLevel);
+
+    $year = (double)$parts[3];
+    $month = (int)$parts[4];
+    $day = (int)$parts[5];
+    $hour = (int)$parts[6];
+    $minute = (int)$parts[7];
+
+    if($parts[1]){$year *= -1;}
+
+    $this->setDate($year,$month,$day);
+    $this->setTime($hour,$minute,0);
+
+    return true;
+  }
+
+  public function toBigInt(){
+    $calendar = self::BI_CALENDAR_GREGORIAN;
+
+    $year = $this->format('Y');
+    $before = (substr($year,0,1) === '-') ? '-' : '';
+    if($before){$year = substr($year,1);}
+    $year = sprintf("%010d",$year);
+
+    return $before.$calendar.$year.$this->format('mdHi');
+  }
+}
