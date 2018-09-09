167bba841fd0d8132ce2863503bb8429fcdcf776
3b-fly <member@3b-fly.eu>
2018-09-09 11:51:09 +0200

    Added new MIME types.

--- build/common/bbbfly_MIME.php
+++ build/common/bbbfly_MIME.php
@@ -12,9 +12,30 @@ class bbbfly_MIME
   const BIN = 'application/octet-stream';
 
   const TEXT = 'text/plain';
+
   const HTML = 'text/html';
   const CSS = 'text/css';
   const JAVASCRIPT = 'application/javascript';
+
+  const PDF = 'application/pdf';
+  const MSWORD = 'application/msword';
+  const WORD_DOCUMENT = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+  const WORD_DOCUMENT_TEMPLATE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.template';
+  const OPEN_TEXT = 'application/vnd.oasis.opendocument.text';
+  const OPEN_TEXT_TEMPLATE = 'application/vnd.oasis.opendocument.text-template';
+
+  const EXCEL = 'application/vnd.ms-excel';
+  const EXCEL_SHEET = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+  const EXCEL_SHEET_TEMPLATE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.template';
+  const OPEN_SHEET = 'application/vnd.oasis.opendocument.spreadsheet';
+  const OPEN_SHEET_TEMPLATE = 'application/vnd.oasis.opendocument.spreadsheet-template';
+
+  const MSPOWERPOINT = 'application/mspowerpoint';
+  const OPEN_PRESENTATION = 'application/vnd.oasis.opendocument.presentation';
+  const OPEN_PRESENTATION_TEMPLATE = 'application/vnd.oasis.opendocument.presentation-template';
+
+  const JSON = 'application/json';
+  const XML = 'application/xml';
 
   const BMP = 'image/bmp';
   const PNG = 'image/png';
@@ -24,21 +45,10 @@ class bbbfly_MIME
   const SVG = 'image/svg+xml';
   const ICO = 'image/vnd.microsoft.icon';
 
-  const PDF = 'application/pdf';
-  const MSWORD = 'application/msword';
-  const EXCEL = 'application/vnd.ms-excel';
-  const WORD_DOCUMENT = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-  const WORD_TEMPLATE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.template';
-  const EXCEL_SHEET = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-  const EXCEL_TEMPLATE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.template';
-
-  const JSON = 'application/json';
-  const XML = 'application/xml';
-
-  const ZIP = 'application/zip';
-  const RAR = 'application/x-rar-compressed';
-  const EXE = 'application/x-msdownload';
-  const MSI = 'application/x-msdownload';
+  const RAW = 'image/x-dcraw';
+  const WAV = 'audio/x-wav';
+  const WAVE = 'audio/wave';
+  const WMA = 'audio/x-ms-wma';
 
   const AVI = 'video/avi';
   const MP3 = 'video/mpeg';
@@ -47,6 +57,11 @@ class bbbfly_MIME
   const MOV = 'video/quicktime';
   const MKV = 'video/x-matroska';
 
+  const ZIP = 'application/zip';
+  const RAR = 'application/x-rar-compressed';
+  const EXE = 'application/x-msdownload';
+  const MSI = 'application/x-msdownload';
+
   protected static function map(){
     return array(
       'txt' => self::TEXT,
@@ -54,6 +69,27 @@ class bbbfly_MIME
       'html' => self::HTML,
       'css' => self::CSS,
       'js' => self::JAVASCRIPT,
+      'pdf' => self::PDF,
+      'doc' => self::MSWORD,
+      'dot' => self::MSWORD,
+      'docx' => self::WORD_DOCUMENT,
+      'dotx' => self::WORD_DOCUMENT_TEMPLATE,
+      'odt' => self::OPEN_TEXT,
+      'ott' => self::OPEN_TEXT_TEMPLATE,
+      'xls' => self::EXCEL,
+      'xlt' => self::EXCEL,
+      'xla' => self::EXCEL,
+      'xlsx' => self::EXCEL_SHEET,
+      'xltx' => self::EXCEL_SHEET_TEMPLATE,
+      'ods' => self::OPEN_SHEET,
+      'ots' => self::OPEN_SHEET_TEMPLATE,
+      'pps' => self::MSPOWERPOINT,
+      'ppt' => self::MSPOWERPOINT,
+      'odp' => self::OPEN_PRESENTATION,
+      'otp' => self::OPEN_PRESENTATION_TEMPLATE,
+      'ini' => self::TEXT,
+      'json' => self::JSON,
+      'xml' => self::XML,
       'bmp' => self::BMP,
       'png' => self::PNG,
       'jpe' => self::JPG,
@@ -64,29 +100,20 @@ class bbbfly_MIME
       'tif' => self::TIF,
       'svg' => self::SVG,
       'ico' => self::ICO,
-      'pdf' => self::PDF,
-      'doc' => self::MSWORD,
-      'dot' => self::MSWORD,
-      'xls' => self::EXCEL,
-      'xlt' => self::EXCEL,
-      'xla' => self::EXCEL,
-      'docx' => self::WORD_DOCUMENT,
-      'dotx' => self::WORD_TEMPLATE,
-      'xlsx' => self::EXCEL_SHEET,
-      'xltx' => self::EXCEL_TEMPLATE,
-      'ini' => self::TEXT,
-      'json' => self::JSON,
-      'xml' => self::XML,
-      'zip' => self::XML,
-      'rar' => self::RAR,
-      'exe' => self::EXE,
-      'msi' => self::MSI,
+      'raw' => self::RAW,
+      'wav' => self::WAV,
+      'wave' => self::WAVE,
+      'wma' => self::WMA,
       'avi' => self::AVI,
       'mp3' => self::MP3,
       'mp4' => self::MP4,
       'flv' => self::FLV,
       'mov' => self::MOV,
-      'mkv' => self::MKV
+      'mkv' => self::MKV,
+      'zip' => self::XML,
+      'rar' => self::RAR,
+      'exe' => self::EXE,
+      'msi' => self::MSI
     );
   }
 
--- src/common/bbbfly_MIME.php
+++ src/common/bbbfly_MIME.php
@@ -4,9 +4,30 @@ class bbbfly_MIME
   const BIN = 'application/octet-stream';
 
   const TEXT = 'text/plain';
+
   const HTML = 'text/html';
   const CSS = 'text/css';
   const JAVASCRIPT = 'application/javascript';
+
+  const PDF = 'application/pdf';
+  const MSWORD = 'application/msword';
+  const WORD_DOCUMENT = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+  const WORD_DOCUMENT_TEMPLATE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.template';
+  const OPEN_TEXT = 'application/vnd.oasis.opendocument.text';
+  const OPEN_TEXT_TEMPLATE = 'application/vnd.oasis.opendocument.text-template';
+
+  const EXCEL = 'application/vnd.ms-excel';
+  const EXCEL_SHEET = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+  const EXCEL_SHEET_TEMPLATE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.template';
+  const OPEN_SHEET = 'application/vnd.oasis.opendocument.spreadsheet';
+  const OPEN_SHEET_TEMPLATE = 'application/vnd.oasis.opendocument.spreadsheet-template';
+
+  const MSPOWERPOINT = 'application/mspowerpoint';
+  const OPEN_PRESENTATION = 'application/vnd.oasis.opendocument.presentation';
+  const OPEN_PRESENTATION_TEMPLATE = 'application/vnd.oasis.opendocument.presentation-template';
+
+  const JSON = 'application/json';
+  const XML = 'application/xml';
 
   const BMP = 'image/bmp';
   const PNG = 'image/png';
@@ -16,21 +37,10 @@ class bbbfly_MIME
   const SVG = 'image/svg+xml';
   const ICO = 'image/vnd.microsoft.icon';
 
-  const PDF = 'application/pdf';
-  const MSWORD = 'application/msword';
-  const EXCEL = 'application/vnd.ms-excel';
-  const WORD_DOCUMENT = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-  const WORD_TEMPLATE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.template';
-  const EXCEL_SHEET = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-  const EXCEL_TEMPLATE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.template';
-
-  const JSON = 'application/json';
-  const XML = 'application/xml';
-
-  const ZIP = 'application/zip';
-  const RAR = 'application/x-rar-compressed';
-  const EXE = 'application/x-msdownload';
-  const MSI = 'application/x-msdownload';
+  const RAW = 'image/x-dcraw';
+  const WAV = 'audio/x-wav';
+  const WAVE = 'audio/wave';
+  const WMA = 'audio/x-ms-wma';
 
   const AVI = 'video/avi';
   const MP3 = 'video/mpeg';
@@ -38,6 +48,11 @@ class bbbfly_MIME
   const FLV = 'video/x-flv';
   const MOV = 'video/quicktime';
   const MKV = 'video/x-matroska';
+
+  const ZIP = 'application/zip';
+  const RAR = 'application/x-rar-compressed';
+  const EXE = 'application/x-msdownload';
+  const MSI = 'application/x-msdownload';
 
   protected static function map(){
     return array(
@@ -47,7 +62,32 @@ class bbbfly_MIME
       'html' => self::HTML,
       'css' => self::CSS,
       'js' => self::JAVASCRIPT,
-      //images
+      //document
+      'pdf' => self::PDF,
+      'doc' => self::MSWORD,
+      'dot' => self::MSWORD,
+      'docx' => self::WORD_DOCUMENT,
+      'dotx' => self::WORD_DOCUMENT_TEMPLATE,
+      'odt' => self::OPEN_TEXT,
+      'ott' => self::OPEN_TEXT_TEMPLATE,
+      //spreadsheet
+      'xls' => self::EXCEL,
+      'xlt' => self::EXCEL,
+      'xla' => self::EXCEL,
+      'xlsx' => self::EXCEL_SHEET,
+      'xltx' => self::EXCEL_SHEET_TEMPLATE,
+      'ods' => self::OPEN_SHEET,
+      'ots' => self::OPEN_SHEET_TEMPLATE,
+      //presentation
+      'pps' => self::MSPOWERPOINT,
+      'ppt' => self::MSPOWERPOINT,
+      'odp' => self::OPEN_PRESENTATION,
+      'otp' => self::OPEN_PRESENTATION_TEMPLATE,
+      //data
+      'ini' => self::TEXT,
+      'json' => self::JSON,
+      'xml' => self::XML,
+      //image
       'bmp' => self::BMP,
       'png' => self::PNG,
       'jpe' => self::JPG,
@@ -58,33 +98,23 @@ class bbbfly_MIME
       'tif' => self::TIF,
       'svg' => self::SVG,
       'ico' => self::ICO,
-      //docs
-      'pdf' => self::PDF,
-      'doc' => self::MSWORD,
-      'dot' => self::MSWORD,
-      'xls' => self::EXCEL,
-      'xlt' => self::EXCEL,
-      'xla' => self::EXCEL,
-      'docx' => self::WORD_DOCUMENT,
-      'dotx' => self::WORD_TEMPLATE,
-      'xlsx' => self::EXCEL_SHEET,
-      'xltx' => self::EXCEL_TEMPLATE,
-      //data
-      'ini' => self::TEXT,
-      'json' => self::JSON,
-      'xml' => self::XML,
-      //archives
-      'zip' => self::XML,
-      'rar' => self::RAR,
-      'exe' => self::EXE,
-      'msi' => self::MSI,
+      //sound
+      'raw' => self::RAW,
+      'wav' => self::WAV,
+      'wave' => self::WAVE,
+      'wma' => self::WMA,
       //video
       'avi' => self::AVI,
       'mp3' => self::MP3,
       'mp4' => self::MP4,
       'flv' => self::FLV,
       'mov' => self::MOV,
-      'mkv' => self::MKV
+      'mkv' => self::MKV,
+      //archive
+      'zip' => self::XML,
+      'rar' => self::RAR,
+      'exe' => self::EXE,
+      'msi' => self::MSI
     );
   }
 
