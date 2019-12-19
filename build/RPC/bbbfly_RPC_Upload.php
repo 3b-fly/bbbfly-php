e194a3ca13b4fe31ec533f2c168b770614240376
3b-fly <member@3b-fly.eu>
2019-12-19 14:24:16 +0100

    Added "bbbfly_RPC_Upload" class.

---
+++ build/RPC/bbbfly_RPC_Upload.php
@@ -0,0 +1,85 @@
+<?php
+/*!
+ * @author Jan Nejedly support@3b-fly.eu
+ * @copyright Jan Nejedly
+ * @version 1.0.0
+ * @license see license in 'LICENSE' file
+*/
+?>
+<?php
+require_once(dirname(__FILE__).'/bbbfly_RPC.php');
+require_once(dirname(__FILE__).'/../common/bbbfly_MIME.php');
+require_once(dirname(__FILE__).'/../common/bbbfly_Upload.php');
+
+abstract class bbbfly_RPC_Upload extends bbbfly_RPC
+{
+  const ERROR_TYPE_MAIN = 1;
+  const ERROR_TYPE_GENERAL = 2;
+  const ERROR_TYPE_EXTENSION = 3;
+  const ERROR_TYPE_SIZE = 4;
+  const ERROR_TYPE_COUNT = 5;
+  const ERROR_TYPE_BATCH = 6;
+
+  private $_uploadOptions = array();
+
+  function __construct($uploadOptions=null,$options=null,$process=true){
+    if(is_array($uploadOptions)){$this->uploadOptions = $uploadOptions;}
+    parent::__construct($options,$process);
+  }
+
+  public function __get($propName){
+    switch($propName){
+      case 'uploadOptions': return $this->_uploadOptions;
+      default: return parent::__get($propName);
+    }
+  }
+
+  public function __set($propName,$value){
+    switch($propName){
+      case 'uploadOptions':if(is_array($value)){$this->_uploadOptions = $value;}break;
+      case 'outputType': parent::__set($propName,bbbfly_RPC::OUTPUT_JSON); break;
+      case 'outputMime': parent::__set($propName,bbbfly_MIME::JSON); break;
+      default: parent::__set($propName,$value); break;
+    }
+  }
+
+  protected function doProcessRPC(){
+    $uploader = new bbbfly_Upload($this->uploadOptions);
+
+    $resultData = array();
+    switch($uploader->upload()){
+      case bbbfly_Upload::UPLOAD_ERROR_NONE:
+      case bbbfly_Upload::UPLOAD_ERROR_FILE:
+        $result = $uploader->Result;
+        if(is_array($result)){
+          foreach($result as $fileResult){
+            $resultItem = new stdClass();
+            $resultItem->Name = $fileResult['name'];
+
+            switch($fileResult['error']){
+              case bbbfly_Upload::UPLOAD_ERROR_FILE_NONE:
+                $resultItem->Id = $fileResult['file_id'];
+              break;
+              case bbbfly_Upload::UPLOAD_ERROR_FILE_SIZE:
+                $resultItem->Error = self::ERROR_TYPE_SIZE;
+              break;
+              case bbbfly_Upload::UPLOAD_ERROR_FILE_EXTENSION:
+                $resultItem->Error = self::ERROR_TYPE_EXTENSION;
+              break;
+              default:
+                $resultItem->Error = self::ERROR_TYPE_GENERAL;
+              break;
+            }
+            $resultData[] = $resultItem;
+          }
+        }
+      break;
+      default:
+        $resultData = new stdClass();
+        $resultData->Error = self::ERROR_TYPE_MAIN;
+      break;
+    }
+
+    $this->setResponseData($resultData);
+  }
+}
